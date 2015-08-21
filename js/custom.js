79adc37cad2ad2a87d16a83c06b836a3c973a184
charlie <xscharliexs@gmail.com>
2015-08-20 19:39:43 -0600

    added phpmailer

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,7 @@ jQuery(function () {
     
     /*global jQuery, $*/
 	jQuery(document).ready(function(){
-		
+		$('.info, .error').hide();
 		
 		// Parallax 
 		$('.home-area').parallax("50%", 0.1);
@@ -60,29 +60,38 @@ jQuery(function () {
 	=================================== */
     $("#contactform").submit(function (e) {
         e.preventDefault();
+        $('.error').hide();
         var name = $("#cf-name").val();
         var email = $("#cf-email").val();
-        var message = $("#cf-phone").val();
-        var dataString = 'name=' + name + '&email=' + email + '&phone=' + message;
+        var phone = $("#cf-phone").val();
+        var comment = $("#cf-comment").val();
+        var dataString = 'name=' + name + '&email=' + email + '&phone=' + phone + '&comment=' + comment;
 
+        $("#cf-name, #cf-email, #cf-phone, #cf-comment, #cf-button").fadeOut(250);
         function isValidEmail(emailAddress) {
             var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
             return pattern.test(emailAddress);
         };
-        if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
+        if (isValidEmail(email) && (phone.length > 1) && (name.length > 1)) {
+            $('.info').fadeIn(1000);
             $.ajax({
                 type: "POST",
-                url: "sendmail.php",
+                url: "mailer.php",
                 data: dataString,
                 success: function () {
-                    $('.success').fadeIn(1000);
-                    $('.error').fadeOut(500);
+                    $('.info').removeClass("alert-info").removeClass("info").addClass("alert-success").addClass("success");
+                    $('.msg').html("Mensaje enviado con exito,<br>Pronto te contactaremos.")
+                    
+                    $(".contact-form")[0].reset();
+                    $("#cf-name, #cf-email, #cf-phone, #cf-comment, #cf-button").fadeIn(250);
                 }
             });
         }
         else {
+            $('.success').hide();
+            $('.info').hide();
             $('.error').fadeIn(1000);
-            $('.success').fadeOut(500);
+            $("#cf-name, #cf-email, #cf-phone, #cf-comment, #cf-button").fadeIn(250);
         }
         return false;
     });	
